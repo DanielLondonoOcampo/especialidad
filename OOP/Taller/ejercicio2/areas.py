762e3be15f1da6003a6f480c1ba70d460bd6d6be
Daniel Londono <daniel.londono.o@hotmail.com>
2024-03-01 15:16:45 -0500

    Ejercicios pendientes completados, boilerplate del ejercicio 7

--- OOP/Taller/ejercicio2/areas.py
+++ OOP/Taller/ejercicio2/areas.py
@@ -5,7 +5,7 @@ def main():
     while True:
         shape = input("Defina la figura para concer su área, puede ser circulo(C), rectangulo(R) o cubo(Q): ").lower()
         match (shape):
-            case "circulo", "c":
+            case "circulo" | "c":
                 try:
                     radio = float(input("Indique el radio de la circunferencia: "))
                     print(f"Perimetro: {2 * pi * radio}, área: {pi * radio**2}")
@@ -13,15 +13,15 @@ def main():
                 except ValueError:
                     print("Valor inválido")
                     continue
-            case "rectangulo", "r":
+            case "rectangulo" | "r":
                 try:
-                    base, altura = to_float(input("Indique base y altura separando por comas: ").split(","))
+                    base, altura = map(float, input("Indique base y altura separando por comas: ").split(","))
                     print(f"Área: {base * altura}")
                     break
                 except ValueError:
                     print("Valor inválido")
                     continue
-            case "cubo", "q":
+            case "cubo" | "q":
                 try:
                     arista = float(input("Indique la longitud de la arista: "))
                     print(f"Volumen: {arista**3}")
@@ -31,10 +31,6 @@ def main():
                     continue
             case _:
                 print("Figura inválida")
-
-
-def to_float(*args):
-    return map(float, args)
 
 
 if __name__ == "__main__":
--- OOP/Taller/ejercicio3/calculadora.py
+++ OOP/Taller/ejercicio3/calculadora.py
@@ -1,31 +1,27 @@
 def main():
     while True:
         try:
-            numbers = to_float(input("Ingrese dos números separados por coma para realizar la operación: ").split(","))
+            a, b = map(float, input("Ingrese dos números separados por coma para realizar la operación: ").split(","))
         except (ValueError, TypeError):
             print("Número inválido")
             continue
         operacion = input("Ingrese la operación deseada, suma(S), resta(R), multiplicación(M), división(D): ").lower()
         match operacion:
-            case "suma", "s":
-                print(f"Resultado de la suma: {numbers[0] + numbers[1]}")
+            case "suma" | "s":
+                print(f"Resultado de la suma: {a + b}")
                 break
-            case "resta", "r":
-                print(f"Resultado de la suma: {numbers[0] - numbers[1]}")
+            case "resta" | "r":
+                print(f"Resultado de la suma: {a - b}")
                 break
-            case "multiplicacion", "s":
-                print(f"Resultado de la multiplicación: {numbers[0] * numbers[1]}")
+            case "multiplicacion" | "m":
+                print(f"Resultado de la multiplicación: {a * b}")
                 break
-            case "division", "s":
-                print(f"Resultado de la división: {numbers[0] / numbers[1]}")
+            case "division" | "d":
+                print(f"Resultado de la división: {a / b}")
                 break
             case _:
                 print("Operación no soportada")
                 continue
-
-
-def to_float(*args):
-    return [float(arg) for arg in args]
 
 
 
