e0071ed118c5e1c2ef9a78246814186e57523b8a
Daniel Londono <daniel.londono.o@hotmail.com>
2024-03-01 10:17:13 -0500

    Ejercicio 2 implementado mas no terminado

---
+++ OOP/Taller/ejercicio2/areas.py
@@ -0,0 +1,35 @@
+from math import pi
+
+def main():
+    
+    while True:
+        shape = input("Defina la figura para concer su área, puede ser circulo(C), rectangulo(R) o cubo(Q): ").lower()
+        match (shape):
+            case "circulo", "c":
+                try:
+                    radio = float(input("Indique el radio de la circunferencia: "))
+                    print(f"Perimetro: {2 * pi * radio}, área: {pi * radio**2}")
+                    break
+                except ValueError:
+                    print("Valor inválido")
+            case "rectangulo", "r":
+                try:
+                    base, altura = float(input("Indique base y altura separando por comas: ").split(","))
+                    print(f"Área: {base * altura}")
+                    break
+                except ValueError:
+                    print("Valor inválido")
+            case "cubo", "q":
+                try:
+                    arista = float(input("Indique la longitud de la arista: "))
+                    print(f"Volumen: {arista**3}")
+                    break
+                except ValueError:
+                    print("Valor inválido")
+            case _:
+                print("Figura inválida")
+
+
+
+if __name__ == "__main__":
+    main()
