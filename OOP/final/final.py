7b55f64432fca49e0a2ac6428a2c9a3a1e041703
Daniel Londoño Ocampo <118467911+DanielLondonoOcampo@users.noreply.github.com>
2024-04-11 17:36:57 -0500

    Update final.py

--- OOP/final/final.py
+++ OOP/final/final.py
@@ -5,57 +5,182 @@ from datetime import datetime
 date_string = datetime.now().strftime("%Y-%m-%d")
 
 class Dorgueria:
-    def __init__(self, medicamentos = [], clientes=[]) -> None:
-        self.__medicamentos = self.set_medicamentos(medicamentos)
+    def __init__(self, clientes=[]) -> None:
         self.__clientes = clientes
 
     def __str__(self):
         pass
 
-    # Getters y setters
-    def get_medicamentos(self):
-        return self.__medicamentos
+    @property
+    def clientes(self):
+        return self.__clientes
 
-    def set_medicamentos(self, medicamentos):
-        try:
-            for medicamento in medicamentos:
-                if isinstance(medicamento, Medicamento) or not medicamentos:
-                    continue
+    @clientes.setter
+    def clientes(self, clientes):
+        try: 
+            if isinstance(clientes, list):
+                for cliente in clientes:
+                    if isinstance(cliente, Cliente):
+                        self.__clientes.append(cliente)
+                    else:
+                        raise ValueError("Cliente inválido")
+            else:
+                if isinstance(clientes, Cliente):
+                    self.__clientes.append(clientes)
                 else:
-                    raise ValueError(f"El elemento: '{medicamento}' no es un medicamento válido")
-            return medicamentos
+                    raise ValueError("Cliente inválido")
         except ValueError as e:
             print(e)
 
-    def get_clientes(self):
-        return self.__clientes
-    
-    # def set_clientes(self, paciente):
-    #     self.__clientes.append(paciente)
-    
     # Metodos del negocio
     def simulate_loading():
-        loading_str = "Loading"
+        loading_str = "Validando venta"
         dots = 0
-        start_time = time.time()  # record the current time
+        start_time = time.time() 
         elapsed_time = 0
-        while elapsed_time < 3:
+        while elapsed_time < 4:
             print(loading_str + "." * dots, end="\r")
-            time.sleep(0.5)  # pause for half a second
-            dots = (dots + 1) % 4  # cycle through 0, 1, 2, 3 dots
-            elapsed_time = time.time() - start_time  # calculate the elapsed time
+            time.sleep(0.5)  
+            dots = (dots + 1) % 4  
+            elapsed_time = time.time() - start_time 
 
-    def generar_factura(self):
-        pass
+    def generar_factura(self, cliente):
+        return f"""
+                    Fecha: {date_string}\n
+                    Nombre del cliente: {cliente.nombre}\n
+                    Número de cédula: {cliente.cedula}\n
+                    Telefono: {cliente.telefono}\n
+                    Dirección: {cliente.direccion}\n\n
+                    Su compra: {cliente.conteo()}\n
+
+                """
 
     def realizar_venta(self):
+        for cliente in self.__clientes:
+            factura = self.generar_factura(cliente)
+            print("Medicamentos antes de la venta")
+            map(print, cliente.medicamentos)
+            for medicamento in cliente.medicamentos:
+                medicamento.disminuir_disponibles()
+            with open("ventas.txt", "a") as file:
+                file.write(factura)
+            print(factura)
+            self.simulate_loading()
+            print("Medicamentos después de la venta")
+            map(print, cliente.medicamentos)
+
+
+class Cliente:
+    def __init__(self, cedula, nombre, telefono, direccion, medicamentos = []) -> None:
+        self.__cedula = cedula
+        self.__nombre = nombre
+        self.__telefono = telefono
+        self.__direccion = direccion
+        self.__medicamentos = medicamentos
         pass
+    
+    @property
+    def cedula(self):
+        return self.__cedula
+
+    @cedula.setter
+    def cedula(self, cedula):
+        try:
+            if cedula.isnumeric():
+                self.__cedula = cedula
+            else:
+                raise ValueError("Número de cédula incorrecto")
+        except ValueError as e:
+            print(e)
+    
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @nombre.setter
+    def nombre(self, nombre):
+        try:
+            if nombre.isalpha():
+                self.__nombre = nombre
+            else:
+                raise ValueError("Nombre incorrecto")
+        except ValueError as e:
+            print(e)
+    
+    @property
+    def telefono(self):
+        return self.__telefono
+
+    @telefono.setter
+    def telefono(self, telefono):
+        try:
+            if telefono.isnumeric():
+                self.__telefono = telefono
+            else:
+                raise ValueError("Número de telefono incorrecto")
+        except ValueError as e:
+            print(e)
+    
+    @property
+    def direccion(self):
+        return self.__direccion
+
+    @direccion.setter
+    def direccion(self, direccion):
+        try:
+            if direccion.isalnum():
+                self.__direccion = direccion
+            else:
+                raise ValueError("Dirección incorrecto")
+        except ValueError as e:
+            print(e)
+    
+    @property
+    def medicamentos(self):
+        return self.__medicamentos
+
+    @medicamentos.setter
+    def medicamentos(self, medicamentos):
+        try:
+            if isinstance(medicamentos, list):
+                for medicamento in medicamentos:
+                    if isinstance(medicamento, Medicamento):
+                        self.__medicamentos.append(medicamento)
+                    else:
+                        raise ValueError(f"El elemento no es un medicamento válido")
+            else:
+                if isinstance(medicamentos, Medicamento):
+                    self.__medicamentos.append(medicamento)
+                else:
+                    raise ValueError(f"El elemento no es un medicamento válido")
+        except ValueError as e:
+            print(e)
+
+    # Método de clase
+    def conteo(self):
+        conteo = 0
+        final = ""
+        total_pago = 0
+        for medicamento in self.__medicamentos:
+            conteo = self.__medicamentos.count(medicamento)
+            precio_final = medicamento.get_precio + medicamento.get_impuesto
+            final += f"""
+                        Medicamento: {medicamento.get_nombre_comercial} ({medicamento.get_nombre_generico}) x{conteo}\n
+                        {f'Medicamento de venta restringida, médico quien ordena: {medicamento.info_medico()}. Dosis máxima {medicamento.get_dosis_maxima}\n' if isinstance(medicamento, Restringido) else f'Contraindicaciones: {medicamento.get_contraindicaciones}\n'}
+                        Miligramos por dosis: {medicamento.get_gramos}\n
+                        Precio: {precio_final}\n\n
+                    """
+            conteo = 0
+            total_pago += (precio_final)
+        final += f"\nTotal a pagar: {total_pago}"
+        return final
+        
 
 
 class Medicamento:
     def __init__(self, sku, nombre_comercial, nombre_generico, precio, impuesto, gramos, unidades_disponibles) -> None:
         self.__sku = self.set_sku(sku)
-        self.__nombre_comercial = self.set_nombre_comervial(nombre_comercial)
+        self.__nombre_comercial = self.set_nombre_comercial(nombre_comercial)
         self.__nombre_generico = self.set_nombre_generico(nombre_generico)
         self.__precio = self.set_precio(precio)
         self.__impuesto = self.set_impuesto(impuesto)
@@ -147,6 +272,7 @@ class Medicamento:
         except ValueError:
             print("Numero de unidades disponibles incorrecto")
 
+    # Método de clase
     def disminuir_disponibles(self):
         self.__unidades_disponibles -= 1
 
@@ -155,19 +281,16 @@ class Restringido(Medicamento):
     def __init__(self, sku, nombre_comercial, nombre_generico, precio, impuesto, gramos, unidades_disponibles, medico, dosis_maxima) -> None:
         super().__init__(sku, nombre_comercial, nombre_generico, precio, impuesto, gramos, unidades_disponibles)
         self.__medico = medico
-        self.__dosis_maxima = self.set_dosis_maxima(dosis_maxima)
+        self.__dosis_maxima = dosis_maxima
 
     def get_medico(self):
         return self.__medico
     
     def get_dosis_maxima(self):
         return self.__dosis_maxima
-    
-    def set_dosis_maxima(self, dosis_maxima):
-        try:
-            return float(dosis_maxima)
-        except ValueError:
-            print("Dosis máxima incorrecta")
+
+    def info_medico(self):
+        return f"{self.__medico["nombre"]}, Telefono: {self.__medico["telefono"]}, Especialidad: {self.__medico["especialidad"]}"
 
 
 class Libre(Medicamento):
@@ -180,8 +303,10 @@ class Libre(Medicamento):
     
 
 # %%Simulacro de venta
-clientes = {
-    "cliente1" : {"cedula" : 12345, "nombre": "Carlos Obando", "telefono": "3215476", "direccion": "Calle falsa con privet drive - 85"},
-    "cliente2" : {"cedula" : 123456, "nombre": "Jessica Obando", "telefono": "3215476", "direccion": "Calle falsa con privet drive - 85"},
-    "cliente3" : {"cedula" : 1234567, "nombre": "Ivan Obando", "telefono": "3215476", "direccion": "Calle falsa con privet drive - 85"}
-}
+medico1 = {"nombre": "Daniel Londoño", "telefono": "3335578", "especialidad": "Medicina general"}
+medico2 = {"nombre": "Ivan Bustillo", "telefono": "3335578", "especialidad": "Oncologia"}
+medico3 = {"nombre": "Andredi Pumarejo", "telefono": "3335578", "especialidad": "Medicina interna"}
+
+
+alprazolam = Restringido(1004729, "Neupax", "Alprazolam", 14000, "16%", 0.25, 7, medico1, "Máximo 10 mg diarios")
+ketamina = Restringido(87659, "Ketolar", "Ketamina", 20000, "20%", 50, 20, medico2, "Máximo 50mg/mL")
